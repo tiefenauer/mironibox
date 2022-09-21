895e42504fc98b26552239f654f1fb12dfee1ed8
Daniel Tiefenauer <daniel.tiefenauer@gmail.com>
2022-09-21 14:13:44 +0200

    cleanup print statements and rename functions

--- src/Buttons.py
+++ src/Buttons.py
@@ -1,15 +1,7 @@
 from datetime import datetime, timedelta
-from enum import Enum
 
 from gpiozero import Button
 from pyee.base import EventEmitter
-
-
-class Events(Enum):
-    NEXT = 'next'
-    PREVIOUS = 'prev'
-    REWIND = 'rewind'
-    PLAY = 'rewind'
 
 
 class Buttons:
@@ -37,17 +29,12 @@ class Buttons:
         self.events.emit("next")
 
     def on_prev_pressed(self):
-        print('on_prev_pressed')
-        print(self.button_prev.pressed_time)
         if self.button_prev.pressed_time:
             if self.button_prev.pressed_time + timedelta(seconds=0.6) > datetime.now():
-                print('emitting prev')
                 self.events.emit('prev')
             else:
-                print('emitting rewind')
                 self.events.emit('rewind')
             self.button_prev.pressed_time = None
         else:
-            print('emitting rewind 2')
             self.button_prev.pressed_time = datetime.now()
             self.events.emit('rewind')
